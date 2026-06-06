1683ead65ad72119ddd63c1f32d2afcfa009d42f
add <samm41236@gmail.com>
2026-06-07 02:05:28 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/AS-portfolio/',
     server: {
         allowedHosts: ['as-portfolio-main-b16649f.kuberns.cloud']
     },
